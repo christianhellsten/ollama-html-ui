a02cabface062e9a5ae90042ecf21b564f0ad284
Christian Hellsten <christian.hellsten@aktagon.com>
2024-01-05 21:12:17 +0200

    Close modal on ESC

--- js/Modal.js
+++ js/Modal.js
@@ -5,6 +5,15 @@ export class Modal {
     this.modal = document.getElementById(this.domId);
     this.closeButton = this.modal.querySelector('.button-close');
     this.closeButton.onclick = () => this.hide();
+    this._bindEventListeners();
+  }
+
+  _bindEventListeners() {
+    window.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        this.hide();
+      }
+    });
   }
 
   show() {

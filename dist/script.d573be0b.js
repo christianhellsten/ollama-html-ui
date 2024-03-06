956bdae1bacb58b93dbae661634981366007bdcc
Christian Hellsten <christian.hellsten@aktagon.com>
2024-03-06 13:05:44 +0200

    Update screenshots

--- dist/script.d573be0b.js
+++ dist/script.d573be0b.js
@@ -1978,7 +1978,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55353" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55613" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
